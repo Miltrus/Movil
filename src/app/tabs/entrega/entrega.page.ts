bf0b2b18370ed8f3c3677a92bd0f6597eaf5eb53
jpchavarria5 <jpchavarria5@misena.edu.co>
2023-07-30 20:53:54 -0500

    Comenzando a hacer el modulo de entrega. Firma hecha

---
+++ src/app/tabs/entrega/entrega.page.ts
@@ -0,0 +1,80 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import SignaturePad from 'signature_pad';
+
+@Component({
+  selector: 'app-entrega',
+  templateUrl: './entrega.page.html',
+  styleUrls: ['./entrega.page.scss'],
+})
+export class EntregaPage implements OnInit {
+
+  @ViewChild('canvas', { static: true }) signaturePadElement?: ElementRef;
+  signaturePad: any;
+
+  constructor(
+    private elementRef: ElementRef,
+    private nav: NavController,
+  ) { }
+
+  ngOnInit() {
+  }
+
+  init() {
+    const canvas: any = this.elementRef.nativeElement.querySelector('canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight - 140;
+    console.log(canvas.width, ' -- ', canvas.height);
+
+    if (this.signaturePad) {
+      this.signaturePad.clear();
+    }
+  }
+
+  public ngAfterViewInit(): void {
+    this.signaturePad = new SignaturePad(this.signaturePadElement?.nativeElement, {
+      penColor: 'rgb(0,0,0)',
+      backgroundColor: 'rgb(255,255,255)'
+    });
+    this.signaturePad.clear();
+  }
+
+  saveSignature() {
+    const dataURL = this.signaturePad.toDataURL('image/png');
+    console.log(dataURL);
+    const blob = this.convertBase64toBlob(dataURL);
+    console.log(blob);
+  }
+
+  convertBase64toBlob(dataURL: any): Blob {
+    const base64Prefix = 'data:image/png;base64,';
+    const base64Data = dataURL.substring(base64Prefix.length);
+  
+    const byteCharacters = atob(base64Data);
+    const byteArrays = new Uint8Array(byteCharacters.length);
+  
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteArrays[i] = byteCharacters.charCodeAt(i);
+    }
+  
+    const blob = new Blob([byteArrays], { type: 'image/png' });
+    return blob;
+  }
+
+  isCanvasBlank(): boolean {
+    if (this.signaturePad) {
+      return this.signaturePad.isEmpty() ? true : false;
+    } else {
+      return false;
+    }
+  }
+
+  clearCanvas() {
+    this.signaturePad.clear();
+  }
+
+  async goBack() {
+    this.nav.navigateBack('/tabs/tabs2');
+  }
+
+}
